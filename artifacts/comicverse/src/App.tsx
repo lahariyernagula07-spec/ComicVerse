0cb73de1cddc62b0f6c20f2cbacbd7eb2716e3ae
lahariyernagula07-spec <lahariyernagula07@gmail.com>
2026-08-16 09:37:13 +0530

    Fix published comic route

--- artifacts/comicverse/src/App.tsx
+++ artifacts/comicverse/src/App.tsx
@@ -10,33 +10,48 @@ import ComicDetail from "./pages/comic-detail";
 import NotFound from "./pages/not-found";
 import SignInPage from "./pages/sign-in";
 import SignUpPage from "./pages/sign-up";
+
 export default function App() {
   return (
     <Layout>
       <Switch>
+        {/* Home */}
         <Route path="/" component={Landing} />
 
+        {/* Dashboard */}
         <Route path="/dashboard" component={Dashboard} />
 
+        {/* Characters */}
         <Route path="/characters" component={Characters} />
 
+        {/* Community listing */}
         <Route path="/community" component={Community} />
 
+        {/* Published comic - new comic */}
         <Route path="/editor/new">
           <Editor comicId={null} />
         </Route>
 
+        {/* Edit existing comic */}
         <Route path="/editor/:id">
           {(params) => (
             <Editor comicId={Number(params.id)} />
           )}
         </Route>
 
+        {/* Comic detail */}
         <Route path="/comic/:id" component={ComicDetail} />
-    <Route path="/sign-in" component={SignInPage} />
-    <Route path="/sign-up" component={SignUpPage} />
+
+        {/* Published comics from Community */}
+        <Route path="/community/:id" component={ComicDetail} />
+
+        {/* Authentication */}
+        <Route path="/sign-in" component={SignInPage} />
+
+        <Route path="/sign-up" component={SignUpPage} />
+
+        {/* 404 */}
         <Route component={NotFound} />
-        
       </Switch>
     </Layout>
   );
